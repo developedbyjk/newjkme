135dfb01a6de471e593f5df9adbb8dfc8605b6af
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2023-10-20 05:26:25 +0000

    added nav sections

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import Navbar from './Navbar'
 export default function About(){
     return(
+        <>        
+        <Navbar/>
         <h1>i am about section</h1>
-    )
+        </>
+
+        )
 }
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import {  NavLink } from 'react-router-dom/dist'
 export default function Navbar(){
-    return(
-        <div>
 
-                <NavLink to="/about" >
+    const activeStyle={
+        color : 'green',
+      
+    }
+
+    return(
+        <div className='navbar'>
+
+            <div className="logo">
+                <h3>@developedbyjk</h3>
+            </div>
+            <div className="nav">
+             <NavLink 
+                to="/"
+                style={({isActive})=> isActive ? activeStyle : null} >
+                Home
+                </NavLink>
+                <NavLink 
+                to="/about"
+                style={({isActive})=> isActive ? activeStyle : null} >
                 About
                 </NavLink>
                 <NavLink to="/projects" >
@@ -14,7 +31,7 @@ export default function Navbar(){
                 More
                 </NavLink>
 
-         
+                </div>
               
         </div>
   
