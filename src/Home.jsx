bef7f5031412bc9ab9eb77c90cb2afeff9629f38
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2023-10-20 11:06:50 +0000

    added new things

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
 import Navbar from './Navbar'
+import { data } from './projectdata'
+
+
 export default function Home(){
+
+        console.log(data);
+
+
+let renderProject = '';
+const mapoverproject = data.map((item,key)=>{
+    return(
+        <>
+            <div className="box">
+                    <div className="emoji">{item.emoji}</div>
+                    <div className="name">{item.name}</div>
+                    <div className="desc">{item.desc} </div>
+                    <button className='blackbtn'><a href={item.link}>Try Now!</a></button>
+                </div>
+        </>
+    )
+})
+renderProject = mapoverproject;
+
+
+console.log("render pojrec is" + renderProject)
+
     return(
         <div>
 
@@ -9,8 +34,10 @@ export default function Home(){
                 <h1>Building Awesome<br/>
                 Things with code!
                 </h1>
-                <button>Explore</button>
+                <button className='blackbtn'>Explore</button>
             </div>
+
+{/* THIS IS INTRO SECTION */}
 
             <div className="intro">
 
@@ -25,9 +52,47 @@ export default function Home(){
                     <p>
                     Since  9 grade i was fascinated by  changing colors in html. Turn out that i love to develop cool stuff on web!  
                     </p>
+                    <p>And here i am Messing around the physical 
+                        world with digital Code!
+                    </p>
+                    <button className='blackbtn'>Know more</button>
                 </div>
                 
             </div>
+
+            <div className="skills">
+                <h1>Skills</h1>
+                <div className="pts">
+                    <div className="pt1">
+                    <p>📙Html * * * *</p>
+                    <p>📗Css * * * *</p>
+                    <p>📕Js * * * *</p>
+                    </div>
+                    <div className="pt2">
+                    <p>⚛️React * * * *</p>
+                    <p>🔥Firebase * * * *</p>
+                    <p>🎨Figma * * * *</p>
+                    </div>
+                </div>
+                
+            </div>
+            
+            <h1>Projects</h1>
+            <div className="projects">
+           
+
+            {renderProject}
+           
+                {/* <div className="box">
+                    <div className="emoji">😒</div>
+                    <div className="name">Angry pill</div>
+                    <div className="desc">Turn your  Anger into
+amazing blue shie luster with our new </div>
+                    <button className='blackbtn'>Try now</button>
+                </div> */}
+            </div>
+
+
               
         </div>
   
---
+++ src/projectdata.js
@@ -0,0 +1,27 @@
+export const data = [
+    {
+        name:'AngryPill',
+        emoji:'😒',
+        desc: 'a great website to free you time by our latest pill',
+        link:'www.google.com',
+    },
+    {
+        name:'Color Pallete',
+        emoji:'🎨',
+        desc: 'find a color infor from the best site on the interne',
+        link:'www.google.com',
+    },
+    {
+        name:'AngryPill',
+        emoji:'😒',
+        desc: 'a great website to free you time by our latest pill',
+        link:'www.google.com',
+    },
+    {
+        name:'Color Pallete',
+        emoji:'🎨',
+        desc: 'find a color infor from the best site on the interne',
+        link:'www.google.com',
+    }
+]
+
